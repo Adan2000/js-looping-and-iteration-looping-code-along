f87eff17e9026e202bf7540010b5d58156fff593
Adan Escamilla <adanescamilla580@gmail.com>
2021-02-22 09:35:38 -0800

    commit tree 5dbeef709b59df4a70a2ce4bd82f5964e4976778

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 // Code your solutions in this file
 
-const names = [ 'Lisa', 'Kaitlin', 'Jan' ];
+// function writeCards(names) {
+//     for (let i = 0; i < names.length; i++) {
+//         console.log("Thank you, ${names[i]}, for the wonderful surprise gift!");
+//     }
+//     return names; 
+// }
 
-function writeCards(names) {
-    for (let i = 0; i < names.length; i++) {
-        console.log("Thank you, ${names[i]}, for the wonderful surprise gift!");
+function writeCards(cards, event) {
+    let arr = []
+    for (let i = 0; i < cards.length; i ++) {
+        arr.push(`Thank you, ${cards[i]}, for the wonderful ${event} gift!`)
     }
-    return names; 
+    return arr
 }
 
 
