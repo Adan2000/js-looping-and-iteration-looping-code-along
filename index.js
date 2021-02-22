458ccccf32496f198a5988c43565f3fa4f8de696
Adan Escamilla <adanescamilla580@gmail.com>
2021-02-21 18:40:08 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+
+
+
+function writeCards(name) {
+  let i = 0; 
+  while (i <= name.length) {
+    console.log('Thank you, ${name}, for the wonderful birthday gift!');
+    i++;
+  }
+
+  return name;
+}
+
+function countDown(number){
+    let countup = 0;
+    while (countup <= number) {
+      console.log(countup++);
+    } 
+}
